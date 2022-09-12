6de88ec22971415b094d7ed5e894e3eb5e64fd8d
jbr1987 <89936153+jbr1987@users.noreply.github.com>
2022-09-12 04:34:39 -0500

    Create show.R

---
+++ show.R
@@ -0,0 +1,17 @@
+### Comment
+
+# Operators
++ # add
+- # subdract
+/ # divide
+* # multiply
+^ # exponent
+| # or
+& # and
+< # less than
+> # greater than
+<- # assign
+= # assign
+== # equals
+<= # less than or equal to
+>= # greater than or equal to
